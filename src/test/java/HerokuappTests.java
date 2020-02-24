d90324b6f7b855b514ee307bf73016f6f857071e
Yuliana.Nesterovich <Yuliana.Nesterovich>
2020-02-24 17:53:40 +0300

    Add @AfterTest

--- src/test/java/HerokuappTests.java
+++ src/test/java/HerokuappTests.java
@@ -2,7 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.json.JsonOutput;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -20,6 +22,10 @@ public class HerokuappTests {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
     }
+    @AfterTest
+    public void doAfterTest(){
+        driver.quit();
+    }
 
     @Test
     public void addRemoveElements() {
@@ -32,11 +38,57 @@ public class HerokuappTests {
         element_delete.click();
 
         //коллекция с элементами "delete"
-        List<WebElement> listOfElements = driver.findElements (By.xpath ("//button[text()='Delete']"));
+        List<WebElement> listOfElements = driver.findElements(By.xpath("//button[text()='Delete']"));
         //проверить количество
         assertEquals(listOfElements.size(), 1, "Количество элементов равно 1");
 
-        driver.quit();
     }
 
+    @Test
+    public void Checkboxes() {
+
+        WebElement element = driver.findElement(By.xpath("//*[@type='checkbox']"));
+        if (element.isSelected()) {
+            System.out.println("Checkbox is Togged On");
+        } else {
+            System.out.println("Checkbox is Togged Off");
+        }
+        element.click();
+        if (element.isSelected()) {
+            System.out.println("Checkbox is Togged On");
+        } else {
+            System.out.println("Checkbox is Togged Off");
+        }
+
+    }
+
+    @Test
+    public void CheckDropdown() {
+        driver.get("http://the-internet.herokuapp.com/dropdown");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement dropdownElement = driver.findElement(By.id("dropdown"));
+
+        Select select1 = new Select(dropdownElement);
+//        List<WebElement> listOfElements = driver.findElement(By.id("dropdown")).findElements(By.xpath("//option[text()='Option*']"));
+//        List options = select.getOptions();
+//        List selectedOptions = select.getAllSelectedOptions();
+//        System.out.println(listOfElements);
+
+        select1.selectByValue("1");
+        if (dropdownElement.isSelected()) {
+            System.out.println("Option 1 выбрана");
+            assertEquals(dropdownElement.isSelected(), dropdownElement.isSelected(), "Выбран первый элемент ");
+
+            Select select2 = new Select(dropdownElement);
+            select2.selectByValue("2");
+            dropdownElement.isSelected();
+            assertEquals(dropdownElement.isSelected(), dropdownElement.isSelected(), "Выбран второй элемент ");
+            if (dropdownElement.isSelected()) {
+                System.out.println("Option 2 выбрана");
+            } else {
+                System.out.println("Не выбрана");
+            }
+        }
+
+    }
 }
