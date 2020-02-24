14150e0f2af26121f05d81eedeeb88430d19da02
Yuliana.Nesterovich <Yuliana.Nesterovich>
2020-02-24 18:25:40 +0300

    Test2 Check Checkboxes

--- src/test/java/HerokuappTests.java
+++ src/test/java/HerokuappTests.java
@@ -22,8 +22,9 @@ public class HerokuappTests {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
     }
+
     @AfterTest
-    public void doAfterTest(){
+    public void doAfterTest() {
         driver.quit();
     }
 
@@ -45,50 +46,22 @@ public class HerokuappTests {
     }
 
     @Test
-    public void Checkboxes() {
-
+    public void CheckCheckboxes() {
+        driver.get("http://the-internet.herokuapp.com/checkboxes");
         WebElement element = driver.findElement(By.xpath("//*[@type='checkbox']"));
-        if (element.isSelected()) {
-            System.out.println("Checkbox is Togged On");
-        } else {
+        if (!element.isSelected()) {
             System.out.println("Checkbox is Togged Off");
         }
         element.click();
         if (element.isSelected()) {
             System.out.println("Checkbox is Togged On");
-        } else {
+        }
+        //не работает
+        WebElement element2 = driver.findElement(By.xpath("//input[@checked='true']"));
+        element2.click();
+        if (!element2.isSelected()) {
             System.out.println("Checkbox is Togged Off");
         }
-
-    }
-
-    @Test
-    public void CheckDropdown() {
-        driver.get("http://the-internet.herokuapp.com/dropdown");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        WebElement dropdownElement = driver.findElement(By.id("dropdown"));
-
-        Select select1 = new Select(dropdownElement);
-//        List<WebElement> listOfElements = driver.findElement(By.id("dropdown")).findElements(By.xpath("//option[text()='Option*']"));
-//        List options = select.getOptions();
-//        List selectedOptions = select.getAllSelectedOptions();
-//        System.out.println(listOfElements);
-
-        select1.selectByValue("1");
-        if (dropdownElement.isSelected()) {
-            System.out.println("Option 1 выбрана");
-            assertEquals(dropdownElement.isSelected(), dropdownElement.isSelected(), "Выбран первый элемент ");
-
-            Select select2 = new Select(dropdownElement);
-            select2.selectByValue("2");
-            dropdownElement.isSelected();
-            assertEquals(dropdownElement.isSelected(), dropdownElement.isSelected(), "Выбран второй элемент ");
-            if (dropdownElement.isSelected()) {
-                System.out.println("Option 2 выбрана");
-            } else {
-                System.out.println("Не выбрана");
-            }
-        }
-
     }
 }
+
